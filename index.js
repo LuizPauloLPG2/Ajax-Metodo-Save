0d2a1a8aa9f0d0ed47ec411d5acd3d98c7564d72
Luiz Paulo Gonçalves <37484860+LuizPauloLPG2@users.noreply.github.com>
2018-10-04 17:40:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
     $("#sendData").on("submit", function (e) {
+        e.preventDefault();
         var html = "";
         var erro = document.querySelector("#erro");
-        e.preventDefault();
+        
         var form = $('#sendData')[0];
+        
         //PEGA DO FORM PARA VALIDAR
         var nome = form.nome_profissional.value;
+        
         if (nome == "") {
             html = "<div class='alert alert-danger' role='alert'>CAMPO NOME REQUERIDO</div>";
             erro.innerHTML = html;
             return false;
         }
+        
         var formData = new FormData(form);
+        
         //DEVOLVE PARA O FORM VALIDADO
         formData.set('nome_profissional', nome);
+        
         $.ajax({
             url: "post.php",
             processData: false,
